11f7f713d5481f4a40fe282130c90ca6f72960d0
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2025-08-21 14:43:07 +0600

    init(client): setup client side

---
+++ client/src/main.jsx
@@ -0,0 +1,22 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { RouterProvider } from 'react-router'
+import { router } from './routes/Router.jsx'
+import AuthProvider from './context/AuthProvider.jsx'
+import ThemeProvider from './context/ThemeProvider.jsx'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <AuthProvider>
+          <RouterProvider router={router}></RouterProvider>
+        </AuthProvider>
+      </ThemeProvider>
+    </QueryClientProvider>
+  </StrictMode>,
+)
