4c0a947ab603b79c5c86a805ce39463deea441ed
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2025-09-12 22:10:25 +0600

    feat(axios-secure): add useAxiosSecure hook for secure API requests with authentication handling

---
+++ client/src/hooks/useAxiosSecure.jsx
@@ -0,0 +1,32 @@
+import axios from "axios";
+import useAuth from "./useAuth";
+
+const useAxiosSecure = () => {
+    const { signOutUser} = useAuth();
+
+    // Create an Axios instance with a base URL
+    const axiosInstance = axios.create({
+        baseURL: `${import.meta.env.VITE_API_URL}`,
+        // baseURL: 'http://localhost:3000',
+        withCredentials: true
+    })
+
+    // Add a response interceptor to handle unauthorized access
+    axiosInstance.interceptors.response.use(response => {
+        return response
+    }), (error) => {
+        if(error.response?.status(401) || error.response?.status(403)) {
+            signOutUser()
+            .then(() => {
+                console.log("Unauthorized access, signing out user.");
+            })
+            .catch(error => {
+                console.error("Error signing out user:", error);
+            })
+        }
+        return Promise.reject(error);
+    }
+    return axiosInstance;
+};
+
+export default useAxiosSecure;
